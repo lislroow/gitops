39df00ae2a5719e81bb8710692d0765e9e4d066f
김명구 <myeonggu.kim@kakao.com>
2024-07-18 08:55:08 +0900

    renew

---
+++ mattermost/create-cert.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+DOMAIN=$1
+FILENM=${DOMAIN}
+
+cat <<- EOF > ${FILENM}.cnf
+[req]
+distinguished_name = req_distinguished_name
+x509_extensions = v3_req
+prompt = no
+[req_distinguished_name]
+countryName             = KR
+stateOrProvinceName     = Seoul
+localityName            = Seonyudo
+organizationName        = MK
+organizationalUnitName  = Dev.Team
+CN                      = ${DOMAIN}
+[v3_req]
+keyUsage = critical, digitalSignature, keyAgreement
+extendedKeyUsage = serverAuth
+subjectAltName = @alt_names
+[alt_names]
+DNS.1 = ${DOMAIN}
+EOF
+
+# crt 생성
+# openssl req -x509 -nodes -days 730 -newkey rsa:2048 -keyout ${FILENM}.key -out ${FILENM}.crt -sha256 -config ${FILENM}.cnf
+openssl req -x509 \
+  -nodes \
+  -days 730 \
+  -newkey rsa:2048 \
+  -keyout ${FILENM}.key \
+  -out ${FILENM}.crt \
+  -sha256 \
+  -config ${FILENM}.cnf
