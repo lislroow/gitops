cdffabee4551b13722770d675e3150091a35866c
myeonggu.kim <myeonggu.kim@kakao.com>
2024-08-24 23:37:44 +0900

    scouter-server 이미지 빌드

--- market/docker-images/scouter-server/build.sh
+++ market/docker-images/scouter-server/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 BASE_DIR=$( cd $( dirname $0 ) && pwd -P )
-IMAGE_NAME="scouter-server:latest"
+IMAGE_NAME="market/scouter-server:latest"
 REGISTRY="localhost:5000"
 
 echo "build ${IMAGE_NAME}"
 
-docker build -t ${IMAGE_NAME} .
-docker image tag ${IMAGE_NAME} market/${IMAGE_NAME}
-docker push ${REGISTRY}/market/${IMAGE_NAME}
+docker build -t ${REGISTRY}/${IMAGE_NAME} .
+docker image tag ${IMAGE_NAME} ${IMAGE_NAME}
+docker push ${REGISTRY}/${IMAGE_NAME}
