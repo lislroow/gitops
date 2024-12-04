0b7102cecc995972db8856067c04971073933e31
김명구 <myeonggu.kim@kakao.com>
2024-12-04 14:34:24 +0900

    docker: devops 분리

---
+++ devops/postgresql/01_create_database.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mapfile -t LIST < <(cat <<- EOF
+
+postgres
+sonarqube
+
+EOF
+)
+
+USER_NAME=postgres
+
+for item in ${LIST[@]}; do
+  DATABASE_NAME=$item
+  psql -U $USER_NAME -tc "SELECT 1 FROM pg_database WHERE datname = '${DATABASE_NAME}'" | grep -q 1 \
+    || psql -U $USER_NAME -c "CREATE DATABASE ${DATABASE_NAME}"
+done
