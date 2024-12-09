545624ced6493d08b8892c1287321ee881573f4f
김명구 <myeonggu.kim@kakao.com>
2024-12-09 22:35:38 +0900

    locale: scouter-server, apps

---
+++ docker-images/corretto8-alpine3/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+BASEDIR=$( cd "$( dirname "$0" )" && pwd -P )
+IMAGE_NAME="mgkim/amazoncorretto:8-alpine3.20"
+REGISTRY="docker.mgkim.net:5000"
+
+echo "build ${IMAGE_NAME}"
+
+docker build -t ${REGISTRY}/${IMAGE_NAME} .
+docker push ${REGISTRY}/${IMAGE_NAME}
