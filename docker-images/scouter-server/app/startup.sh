7653039f605d281098c9b7bd46381c6aae7ecbe7
김명구 <myeonggu.kim@kakao.com>
2024-12-10 22:28:48 +0900

    scouter-server: 파일 정리, startup.sh 정리

--- docker-images/scouter-server/app/startup.sh
+++ docker-images/scouter-server/app/startup.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-
-BASE_DIR=$( cd $( dirname $0 ) && pwd -P )
+BASEDIR=$( cd "$( dirname "$0" )" && pwd -P )
 
 if [[ `uname -s` == "MINGW64_NT-"* ]]; then
-  export JAVA_HOME='/c/develop/tools/java/corretto-1.8.0_382'
+  export JAVA_HOME='/c/develop/tools/corretto/corretto-1.8.0_382'
   export PATH="$JAVA_HOME/bin:$PATH"
-  cd $BASE_DIR
 fi
 
-java -Xmx1024m -classpath $BASE_DIR:$BASE_DIR/scouter-server-boot.jar -Dscouter.config=conf/scouter-local.conf scouter.boot.Boot
+CLASSPATH="${BASEDIR}"
+for file in $BASEDIR/lib/*.jar; do
+  CLASSPATH="${CLASSPATH}:${file}"
+done
+CLASSPATH="${CLASSPATH}:${BASEDIR}/scouter-server-boot.jar"
 
+echo "classpath=" $CLASSPATH
+
+java -Xmx1024m -classpath $CLASSPATH -Dscouter.config=conf/scouter.conf scouter.boot.Boot
