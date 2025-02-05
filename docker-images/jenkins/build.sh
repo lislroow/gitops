7eb3e0e380a1e8ff1b58d200bf41233362da3c56
김명구 <myeonggu.kim@kakao.com>
2025-02-06 02:42:40 +0900

    jenkins: podman 추가

--- docker-images/jenkins/build.sh
+++ docker-images/jenkins/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BASEDIR=$( cd "$( dirname "$0" )" && pwd -P )
-IMAGE_NAME="mgkim/amazoncorretto:17-alpine3.20"
+IMAGE_NAME="mgkim/jenkins:2.496-jdk17"
 REGISTRY="localhost:5000"
 
 echo "build ${IMAGE_NAME}"
