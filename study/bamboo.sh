586af9ed80db3540192ce9f121fb3164518f3d7f
김명구 <myeonggu.kim@kakao.com>
2025-09-22 23:05:25 +0900

    [misc] 미사용 제거

--- study/bamboo.sh
+++
@@ -1,5 +0,0 @@
-if [ $(docker volume ls | grep -c bamboo_data) -eq 0 ]; then
-  docker volume create --name bamboo_data
-fi
-
-docker run -v bamboo_data:/var/atlassian/application-data/bamboo --name="bamboo" -d -p 8085:8085 -p 54663:54663 atlassian/bamboo:9.6-jdk17
--- study/bitbucket.sh
+++
@@ -1,5 +0,0 @@
-if [ $(docker volume ls | grep -c bitbucket_data) -eq 0 ]; then
-  docker volume create --name bitbucket_data
-fi
-
-docker run -v bitbucket_data:/var/atlassian/application-data/bitbucket --name="bitbucket" -d -p 7990:7990 -p 7999:7999 atlassian/bitbucket:8.16.3-jdk17
