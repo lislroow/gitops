8b7510c21eccdc1efbbfb20b14f621766b6d97a0
김명구 <myeonggu.kim@kakao.com>
2025-09-24 14:03:21 +0900

    [feature] build 스크립트 개선

--- docker-images/corretto17-alpine3/build.sh
+++ docker-images/corretto17-alpine3/build.sh
@@ -1,10 +1,91 @@
-#!/bin/bash
+#!/usr/bin/env bash
+BASEDIR=$(cd $(dirname $0) && pwd -P)
+SCRIPT_NM="${0##*/}"
+PRIVATE_REGISTRY="docker.mgkim.net"
 
-BASEDIR=$( cd "$( dirname "$0" )" && pwd -P )
-IMAGE_NAME="study/amazoncorretto:17-alpine3.20"
-REGISTRY="localhost:5000"
 
-echo "build ${IMAGE_NAME}"
+# usage
+function USAGE {
+  cat << EOF
+- Usage  $SCRIPT_NM COMMAND
+Commands:
+  build     build image
+  deploy    build & deploy image
 
-docker build -t ${REGISTRY}/${IMAGE_NAME} .
-docker push ${REGISTRY}/${IMAGE_NAME}
+Options:
+  --registry  docker registry domain
+              e.g '$SCRIPT_NM [build | deploy] --registry ${PRIVATE_REGISTRY}'
+
+EOF
+  exit 1
+}
+# //usage
+
+# options
+declare o_registry
+OPTIONS=""
+LONGOPTIONS="registry:"
+opts=$(getopt --options "${OPTIONS}" \
+              --longoptions "${LONGOPTIONS}" \
+              -- "$@" )
+eval set -- "${opts}"
+while true; do
+  [ -z "$1" ] && break
+  
+  case "$1" in
+    --registry)
+      o_registry="$2"
+      shift
+      ;;
+    *)
+      argv+=($1)
+      ;;
+  esac
+  shift
+done
+# -- options
+
+# validate
+[ "${#argv[@]}" -eq 0 ] && USAGE
+# -- validate
+
+
+registry="${o_registry:-$PRIVATE_REGISTRY}"
+image="amazoncorretto"
+tag="17-alpine3.20"
+
+[ -z "${registry}" ] && { echo "registry must not empty."; exit 1; }
+
+
+build() {
+  printf "## build\n"
+  docker build -t ${registry}/${image}:${tag} .
+  echo ""
+}
+
+deploy() {
+  printf "## deploy\n"
+  
+  printf "### deploy: push image \n"
+  docker push ${registry}/${image}:${tag}
+  printf "### deploy: remove image \n"
+  docker rmi ${registry}/${image}:${tag}
+  echo ""
+}
+
+# main
+command=${argv[1]}
+
+case "${command}" in
+  build)
+    build
+    ;;
+  deploy)
+    build
+    deploy
+    ;;
+  *)
+    USAGE
+    ;;
+esac
+# -- main
