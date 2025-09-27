cd824d0ea8b60f27d5d7ce5f1ade089980760eed
김명구 <myeonggu.kim@kakao.com>
2025-09-27 22:55:20 +0900

    [update] docker.sh 변수/USAGE 정리

--- docker/docker.sh
+++ docker/docker.sh
@@ -4,28 +4,25 @@ SCRIPT_NM="${0##*/}"
 
 shopt -s globstar
 
-up="\033[A"
-clean="\033[K"
-
 # usage
 function USAGE {
   cat << EOF
 - Usage  $SCRIPT_NM COMMAND service [OPTIONS]
 COMMAND:
-  start     Start service
-  stop      Stop service
-  restart   Stop and Start service
-  up        Create service
-  down      Remove service
-  recreate  Remove and Create service
-  status    'docker ps' command
-  logs      Fetch the logs of service
+  up        'create' and 'start'
+  down      'stop' and 'remove'
+  recreate  'down' and 'up'
+  start     'start'
+  stop      'stop'
+  restart   'stop' and 'start'
+  logs      'logs -f -n 10'
+  status    service status
 
 OPTIONS:
-  --list    print available service list
-  --logs    Start command and 'logs -f'
-  --v       'docker-compose down --v' : down container and remove associate volumes
-            'docker-compose stop --v' : stop container and remove associate volumes
+  --show    show available service list
+  --logs    logs after 'start|restart|up|recreate'
+  --v       remove associate volumes 
+            'docker-compose [down|stop] --v service'
 
 EOF
   exit 1
@@ -33,12 +30,12 @@ EOF
 # //usage
 
 # options
-declare o_list
-declare o_logs
-declare o_project
-declare o_rm_vols
+declare show_y
+declare logs_y
+declare project_y
+declare rm_vols_y
 OPTIONS=""
-LONGOPTIONS="list,logs,project,v"
+LONGOPTIONS="show,logs,project,v"
 opts=$(getopt --options "${OPTIONS}" \
               --longoptions "${LONGOPTIONS}" \
               -- "$@" )
@@ -47,17 +44,17 @@ while true; do
   [ -z "$1" ] && break
   
   case "$1" in
-    --list)
-      o_list="y"
+    --show)
+      show_y="y"
       ;;
     --logs)
-      o_logs="y"
+      logs_y="y"
       ;;
     --project)
-      o_project="y"
+      project_y="y"
       ;;
     --v)
-      o_rm_vols="y"
+      rm_vols_y="y"
       ;;
     --)
       ;;
@@ -74,36 +71,38 @@ done
 # -- options
 
 # init
-declare all_entries=($(ls **/*.yml 2> /dev/null | awk '{ origin=$0; sub("\\.yml", "", $0); sub("/yml", "", $0); printf "%s,%s\n", origin, $0 }'))
-# echo "all_entries=(${all_entries[@]})"
-declare -a o_target=("${argv[@]:0:${#argv[@]}-1}")
-# echo "o_target=(${o_target[@]})"
-
-declare entries
-if [ -n "${o_project}" ]; then
-  entries=($(printf "%s\n" "${all_entries[@]}" | grep "^${o_target}/"))
-else
-  o_target_grep=$(IFS='|'; echo "(${o_target[*]})") # caution
-  entries=($(printf "%s\n" "${all_entries[@]}" | grep -E "${o_target_grep}$"))
-fi
-if [ "${o_list}" == "y" ]; then
+declare -a m_all_entries=($(ls **/*.yml 2> /dev/null | awk '{ origin=$0; sub("\\.yml", "", $0); sub("/yml", "", $0); printf "%s,%s\n", origin, $0 }'))
+# echo "m_all_entries=(${m_all_entries[@]})"
+if [ "${show_y}" == "y" ]; then
   printf "* available service list\n"
-  printf "  %s\n" "${entries[@]#*\,}"
+  printf "  %s\n" "${m_all_entries[@]#*\,}"
   exit
 fi
 
-declare command=${argv[-1]}
-if [ -z "${command}" ]; then
+declare p_command=${argv[0]}
+if [ -z "${p_command}" ]; then
   printf "[%-5s] %s\n\n" "ERROR" "COMMAND is required"
   USAGE
 fi
-if [ -z "${o_target}" ]; then
+
+# declare -a p_targets=("${argv[@]:0:${#argv[@]}-1}")
+declare -a p_targets=("${argv[@]:1}")
+# echo "p_targets=(${p_targets[@]})"
+if [ ${#p_targets[@]} -eq 0 ]; then
   printf "[%-5s] %s\n\n" "ERROR" "service or project required"
   USAGE
 fi
-if [ ${#entries[@]} -eq 0 ]; then
+
+declare -a m_entries
+if [ "${project_y}" == "y" ]; then
+  m_entries=($(printf "%s\n" "${m_all_entries[@]}" | grep "^${p_targets}/"))
+else
+  _grep_str=$(IFS='|'; echo "(${p_targets[*]})") # caution
+  m_entries=($(printf "%s\n" "${m_all_entries[@]}" | grep -E "${_grep_str}$"))
+fi
+if [ ${#m_entries[@]} -eq 0 ]; then
   printf "[%-5s] %s\n" "ERROR" " use '--project' or choose one"
-  for e in ${all_entries[@]}; do
+  for e in ${m_all_entries[@]}; do
     echo -n " '${e#*,}'"
   done
   echo ""
@@ -154,40 +153,35 @@ get_depends_file() {
   echo "${compose_file[@]}"
 }
 
-get_running() {
-  local container=$1
-  local running=$(docker inspect --format '{{.State.Running}}' ${container} 2>/dev/null)
-  echo "${running}"
-}
-
 exec_compose() {
-  local cmd="$1"
-  local service="$2"
-  local compose_files=("$3")
-  case "${cmd}" in
+  local command="$1"
+  local project="$2"
+  local service="$3"
+  local compose_files=("$4")
+  case "${command}" in
     start|stop|down)
       ;;
     up)
-      cmd+=" -d"
+      command+=" -d"
       ;;
     logs)
-      cmd+=" -f -n 10"
+      command+=" -f -n 10"
       ;;
     *)
       printf "[%-5s] %s\n\n" "ERROR" "invalid docker-compose command. (avaiable: start, stop, up, down, logs)" 1>&2
       return
       ;;
   esac
-  [ -z "${service}" ] && [[ "${cmd}" != "logs"* ]] && { printf "[%-5s] %s\n\n" "ERROR" "service is required." 1>&2; return; }
-  [ ${#compose_files[@]} -eq 0 ] && [[ "${cmd}" != "logs"* ]] && { printf "[%-5s] %s\n\n" "ERROR" "compose files are required"; return; }
+  [ -z "${service}" ] && [[ "${command}" != "logs"* ]] && { printf "[%-5s] %s\n\n" "ERROR" "service is required." 1>&2; return; }
+  [ ${#compose_files[@]} -eq 0 ] && [[ "${command}" != "logs"* ]] && { printf "[%-5s] %s\n\n" "ERROR" "compose files are required"; return; }
 
   local compose_opts
   for item in ${compose_files[@]}; do
     compose_opts="${compose_opts} -f ${item}"
   done
   
-  echo "docker-compose -p ${project} ${compose_opts:1} ${cmd} ${service}"
-  docker-compose -p ${project} ${compose_opts:1} ${cmd} ${service}
+  echo "docker-compose -p ${project} ${compose_opts:1} ${command} ${service}"
+  docker-compose -p ${project} ${compose_opts:1} ${command} ${service}
 }
 
 volume() {
@@ -241,7 +235,8 @@ status() {
         ;;
     esac
   done
-  
+  local running=$(docker inspect --format '{{.State.Running}}' ${container} 2>/dev/null)
+
   echo ""
   echo "## volumes"
   volume
@@ -252,29 +247,29 @@ status() {
 # -- functions
 
 # variables
-declare project
-declare -a compose_files
-declare env_file
-declare -a service_list=()
+declare -a m_services=()
 # -- variables
 
 # main
 ## process each service individually
-for entry in "${entries[@]}"; do
+declare -i tot=${#m_entries[@]}
+declare -i idx
+for entry in ${m_entries[@]}; do
   declare target="${entry#*,}"
-  project="${target%/*}"
+  declare project="${target%/*}"
   declare service="${target#*/}"
-  service_list+=("${service}")
-  env_file="${BASEDIR}/${project}/.env"
+  m_services+=("${service}")
+  declare env_file="${BASEDIR}/${project}/.env"
   declare compose_file="${BASEDIR}/${entry%,*}"
 
-  compose_files=($(get_depends_file "${project}" "${compose_file}"))
+  declare -a compose_files=($(get_depends_file "${project}" "${compose_file}"))
   if [[ "${compose_files[@]}" != *" ${compose_file} "* ]]; then
     compose_files+=(${compose_file})
   fi
 
+  ((idx++))
   cat <<-EOF
-* target '${target}'
+* [${idx}/${tot}] target '${target}'
   project   : ${project}
   service   : ${service}
   env       : ${env_file}
@@ -291,40 +286,40 @@ EOF
   fi
   
   compose_files="${compose_files[@]}" # joined
-  case "${command}" in
+  case "${p_command}" in
     start|stop|up|down)
-      exec_compose "${command}" "${service}" "${compose_files}"
+      exec_compose "${p_command}" "${project}" "${service}" "${compose_files}"
       ;;
     restart)
-      exec_compose "stop" "${service}" "${compose_files}"
-      exec_compose "start" "${service}" "${compose_files}"
+      exec_compose "stop" "${project}" "${service}" "${compose_files}"
+      exec_compose "start" "${project}" "${service}" "${compose_files}"
       ;;
     recreate)
-      exec_compose "down" "${service}" "${compose_files}"
-      exec_compose "up" "${service}" "${compose_files}"
+      exec_compose "down" "${project}" "${service}" "${compose_files}"
+      exec_compose "up" "${project}" "${service}" "${compose_files}"
       ;;
   esac
 done
 
 ## process all services together
-case "${command}" in
+case "${p_command}" in
   logs)
     sleep 0.3
-    exec_compose "logs" "${service_list[*]}"
+    exec_compose "logs" "${project}" "${m_services[*]}"
     ;;
   start|restart|up|recreate)
-    if [ "${o_logs}" == "y" ]; then
+    if [ "${logs_y}" == "y" ]; then
       sleep 0.5
-      exec_compose "logs" "${service_list[*]}"
+      exec_compose "logs" "${project}" "${m_services[*]}"
     fi
     ;;
   status)
     sleep 0.3
-    # exec_compose "status" "${service_list[*]}"
+    # exec_compose "status" "${m_services[*]}"
     ;;
   volume)
     sleep 0.3
-    # exec_compose "volume" "${service_list[*]}"
+    # exec_compose "volume" "${m_services[*]}"
     ;;
 esac
 # -- main
