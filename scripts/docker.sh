cd2b435ba3d5e29418c998e4beddaeb42eb5c5fa
김명구 <myeonggu.kim@kakao.com>
2025-10-02 00:15:34 +0900

    [misc] docker.sh: 코드 정리

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -29,10 +29,13 @@ EOF
 function LIST {
   declare -a files=(**/*.yml)
   declare -i file_cnt=${#files[@]}
-  printf "* available list (${file_cnt})\n"
+  # printf "* available list (${file_cnt})\n"
   
+  local output=""
   declare -i f3_len=$(printf "%s\n" ${files[@]} | wc -L)
-  declare output=""
+  local FORMAT="  %2s  %-7s  %-$((f3_len+2))s  %s\n"
+  output+=$(printf "${FORMAT}" "NO" "PROJECT" "FILE" "SERVICES")
+  output+="\n"
   for i in $(seq 0 $((file_cnt-1))); do
     yml_file=${files[$i]}
     if [ $(echo "${yml_file}" | grep -o '/' | wc -l) -eq 0 ]; then
@@ -42,7 +45,7 @@ function LIST {
     fi
     declare -a services=($(yq '.services | keys | .[]' $yml_file))
     service_list="$(IFS=,; echo "${services[*]}")"
-    output+=$(printf "  %2s | %-6s | %-$((f3_len+2))s | %s\n" "$((i+1))" "${project}" "${yml_file}" "${service_list}")
+    output+=$(printf "${FORMAT}" "$((i+1))" "${project}" "${yml_file}" "${service_list}")
     output+="\n"
   done
   echo -e "${output}"
@@ -86,7 +89,8 @@ done
 declare p_command=${argv[0]}
 if [ -z "${p_command}" ]; then
   printf "[%-5s] %s\n\n" "ERROR" "COMMAND is required"
-  USAGE
+  printf " : %s\n" "'${SCRIPT_NM} -h'"
+  exit
 fi
 
 case "${p_command}" in
@@ -98,12 +102,14 @@ esac
 
 declare -a p_targets=("${argv[@]:1}")
 if [ ${#p_targets[@]} -eq 0 ]; then
-  printf "[%-5s] %s\n\n" "ERROR" "service or project is required"
-  USAGE
+  printf "[%-5s] %s\n" "ERROR" "service or project is required. check available list."
+  printf " : %s\n" "'${SCRIPT_NM} -h' or '${SCRIPT_NM} list'"
+  exit
 fi
 
 declare -A g_all_entries
 declare -a g_all_entry_keys=()
+
 fn_all_entries() {
   declare -a all_yml_list=(**/*.yml)
   if (( ${#all_yml_list[@]} > 0 )); then
@@ -126,10 +132,10 @@ fn_all_entries() {
     done
   fi
 }
-
-## all g_entries
 fn_all_entries
 # -- init
+
+
 
 # functions
 exec_compose() {
@@ -166,70 +172,11 @@ exec_compose() {
   echo "docker-compose -p ${project} ${compose_opts:1} ${command} ${detach_opt_y:+-d} ${services[@]}"
   docker-compose -p ${project} ${compose_opts:1} ${command} ${detach_opt_y:+-d} ${services[@]}
 }
-
-volume() {
-  local volume_list=($(awk '/^volumes:/ {flag=1; next}
-    /^[^[:space:]]/ {flag=0}
-    flag {
-      if ($1 == "") next
-      sub(":$", "", $1)
-      print $1
-    }' "${compose_file}"))
-  declare -i max_len=0
-  for item in ${volume_list[@]}; do
-    local volume="${project}_${item}"
-    [ ${max_len} -lt ${#volume} ] && max_len=${#volume}
-  done
-  for item in ${volume_list[@]}; do
-    local volume="${project}_${item}"
-    docker volume inspect ${volume} --format '{{.Mountpoint}}' 2> /dev/null | \
-      awk -v volume="${volume}" -v max_len="${max_len}" '{
-        if ($1 == "") {
-          $1 = "X (not exist)"
-        }
-        fmt = "   %-" max_len "s   %s\n"
-        printf fmt, volume, $1
-      }'
-  done
-}
-
-status() {
-  local list=($(docker-compose ${project_opt} ${file_opt} ps -a | tail -n +2 | awk '{ print $1 }'))
-  echo "## containers"
-  echo " * project: ${project}"
-  declare -i max_len=0
-  declare -a running=()
-  for service in ${list[@]}; do
-    [ $max_len -lt ${#service} ] && max_len=${#service}
-  done
-  
-  for service in ${list[@]}; do
-    local status=$(docker inspect ${service} --format '{{.State.Status}}' 2> /dev/null)
-    case "${status}" in
-      running)
-        printf "   %-${max_len}s   %s\n" "${service}" "${status}"
-        running+=(${service})
-        ;;
-      exited)
-        printf "   %-${max_len}s   %s\n" "${service}" "${status}"
-        ;;
-      *)
-        printf "   %-${max_len}s   unknown status '%s'\n" "${service}" "${status}"
-        ;;
-    esac
-  done
-  local running=$(docker inspect --format '{{.State.Running}}' ${container} 2>/dev/null)
-
-  echo ""
-  echo "## volumes"
-  volume
-  if [ ${#list[@]} -gt 0 ]; then
-    printf "\n"
-  fi
-}
 # -- functions
 
-# main
+
+
+# main process
 declare -A g_entries
 declare -a g_entry_keys=()
 
@@ -310,29 +257,27 @@ fn_entries() {
   done
 
   if (( ${#g_entry_keys[@]} == 0 )); then
-    printf "[%-5s] %s\n" "ERROR" "target is empty"
-    echo ""
-    LIST
+    printf "[%-5s] %s\n" "ERROR" "target is empty. check available list."
+    printf " : %s\n" "'${SCRIPT_NM} list'"
     exit
   fi
 }
 
 fn_process() {
-  ## process each service individually
   declare -i tot=${#g_entry_keys[@]}
   declare -i idx
-  for key in ${g_entry_keys[@]}; do
-    declare project="${key%:*}"
-    declare compose_file="${key#*:}"
-    declare services="${g_entries[$key]}"
-    declare env_file="${project:+$project/}.env"
+  for idx in $(seq 1 $tot); do
+    local key=${g_entry_keys[$((idx-1))]}
+    local project="${key%:*}"
+    local compose_file="${key#*:}"
+    local services="${g_entries[$key]}"
+    local env_file="${project:+$project/}.env"
 
     declare -a compose_files=()
     declare -a dep_compose_files=()
     for dep_service in $(yq '.services[].depends_on | select(. != null) | keys | .[]' ${compose_file}); do
       for key in ${g_all_entry_keys[@]}; do
         if (( $(echo ${g_all_entries[$key]} | grep -o ${dep_service} | wc -l) > 0 )); then
-          echo " > ${key#*:}"
           if [ "${key#*:}" != "${compose_file}" ]; then
             dep_compose_files=(${key#*:})
           fi
@@ -343,16 +288,14 @@ fn_process() {
     compose_files+=(${compose_file})
     compose_files+=(${dep_compose_files[@]})
     
-    ((idx++))
     cat <<-EOF
-  * [${idx}/${tot}] ${key}
-    project    : ${project}
-    services   : ${services}
-    env        : ${env_file}
-    compose    : ${compose_file}
-    depends_on : ${dep_compose_files[@]:-(none)}
+* [${idx}/${tot}] ${key}
+  project    : ${project}
+  services   : ${services}
+  env        : ${env_file}
+  compose    : ${compose_file}
+  depends_on : ${dep_compose_files[@]:-(none)}
 EOF
-
 
     case "${p_command}" in
       start|stop|up|down)
@@ -369,7 +312,7 @@ EOF
     esac
   done
 
-  ## process all services together
+  ## execute 'logs'
   case "${p_command}" in
     logs)
       sleep 0.3
@@ -380,14 +323,6 @@ EOF
         sleep 0.5
         exec_compose "logs" "${project}" "${services}"
       fi
-      ;;
-    status)
-      sleep 0.3
-      # exec_compose "status" "${services}"
-      ;;
-    volume)
-      sleep 0.3
-      # exec_compose "volume" "${services}"
       ;;
   esac
 }
